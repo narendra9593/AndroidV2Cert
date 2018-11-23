24c60694a397038605a19ed9f524fb0638b4eb50
NarendraPrajapati <narendra.prajapati@azilen.com>
2018-11-23 23:07:24 +0530

    Initial commit

---
+++ app/src/main/java/com/Unit1Fragment1/SimpleFragment.java
@@ -0,0 +1,54 @@
+package com.Unit1Fragment1;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class SimpleFragment extends Fragment {
+    // The radio button choice has 3 states: 0 = yes, 1 = no,
+    // 2 = default (no choice). Using only 0 and 1.
+    private static final int YES=0;
+    private static final int NO=1;
+
+    public SimpleFragment(){
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        final View rootView=inflater.inflate(R.layout.fragment_simple,container,false);
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+
+        // Set the radioGroup onCheckedChanged listener.
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int id) {
+                View radioButton = radioGroup.findViewById(id);
+                int index = radioGroup.indexOfChild(radioButton);
+                TextView textView =
+                        rootView.findViewById(R.id.fragment_header);
+                switch (index) {
+                    case YES: // User chose "Yes".
+                        textView.setText(R.string.yes_message);
+                        break;
+                    case NO: // User chose "No".
+                        textView.setText(R.string.no_message);
+                        break;
+                    default: // No choice made.
+                        // Do nothing.
+                        break;
+                }
+            }
+        });
+        return rootView;
+    }
+}
